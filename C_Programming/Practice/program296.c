9d6dedd877a54def552b1f12cfe1a8f6f46f674b
Sakshi Dipak Kachare <sakshikachare23@gmail.com>
2026-07-13 01:31:38 +0530

    Problems on String

---
+++ C_Programming/Practice/program296.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 11, j = 21;
+    int temp = 0;
+
+    temp = i;
+    i = j;
+    j = temp;
+
+    printf("%d\n",i);
+    printf("%d\n",j);
+
+
+    return 0;
+}
