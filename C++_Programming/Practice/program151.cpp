aabec7bb6e5add7c1c2aea4a38e3246a4100207a
Sakshi Dipak Kachare <sakshikachare23@gmail.com>
2026-07-15 00:07:47 +0530

    Problems On Array

---
+++ C++_Programming/Practice/program151.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+    // Access Specifier of Summation is PRIVATE
+    int Summation(int Arr[],int iSize)
+    {
+       int iSum = 0, iCnt = 0;
+       for(iCnt = 0; iCnt < iSize; iCnt++)
+       {
+          iSum = iSum + Arr[iCnt];
+       }
+       return iSum;
+    }
+};
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0 , iCnt = 0, iRet =0;
+    ArrayX aobj;
+
+    cout<<"Enter the number of Elements: \n";
+    cin>>iLength;
+
+    Brr = new int[iLength];
+
+    cout<<"Enter the Elements: \n";
+    for(iCnt = 0; iCnt<iLength; iCnt++)
+    {
+        cin>>Brr[iCnt];
+    }
+
+    cout<<"Elements of the Array are: \n";
+    for(iCnt = 0; iCnt<iLength; iCnt++)
+    {
+        cout<<Brr[iCnt]<<endl;
+    }
+
+    iRet = aobj.Summation(Brr,iLength);
+
+    cout<<"Summation is: "<<iRet<<endl;
+    delete []Brr;
+              
+    return 0;
+}
---
+++ C++_Programming/Practice/program159.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+class ArrayX
+{
+    public:
+       int *Arr;
+       int iSize;
+
+       // Parameterized Constructor
+       ArrayX(int X)
+       {
+          cout<<"Inside Constructor"<<endl;
+          iSize = X;               // Characteristics initialization
+          Arr = new int[iSize];    // Resource Allocation
+          
+       }
+
+       // Destructor
+       ~ArrayX()
+       {
+            cout<<"Inside Destructor"<<endl;
+            delete []Arr;           // Resource Deallocation
+
+       }
+
+};
+
+int main()
+{
+    // Static memory allocation for object
+    //ArrayX aobj1(5);  
+
+    ArrayX *aobj1 = new ArrayX(5);
+    
+    return 0;
+}
