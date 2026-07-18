3e675085a71f2748d2035398257d07f2bf380887
Sakshi Dipak Kachare <sakshikachare23@gmail.com>
2026-07-18 11:27:35 +0530

    Problems On Pattern Printing

---
+++ Java_Programming/Practice/program187.java
@@ -0,0 +1,36 @@
+// Input : 7
+// Output : A b C d E f G
+import java.util.*;
+
+class program187
+{
+    public static void Display(int iNo)
+    { 
+        int iCnt = 0 ;
+        char ch1 = '\0', ch2 = '\0';
+        for(iCnt = 1 , ch1 = 'a', ch2 = 'A'; iCnt <= iNo ; iCnt++,ch1++ , ch2++)
+        {
+            if(iCnt % 2 == 0)
+            {
+                System.out.print(ch1+"\t");
+            
+            }
+            else
+            {
+                System.out.print(ch2+"\t");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the number of Elements: ");
+        iValue = sobj.nextInt();
+
+        Display(iValue);
+        
+    }
+}
