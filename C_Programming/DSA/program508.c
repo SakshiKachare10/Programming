3e4ed320dc80f315835767af4ba2a10d796f2d0a
Sakshi Dipak Kachare <sakshikachare23@gmail.com>
2026-08-07 11:59:06 +0530

    Add files via upload

---
+++ C_Programming/DSA/program508.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void Display()
+{
+    auto int i = 1;
+
+    if(i <= 4)
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
