080e661d9be15d056d942dddd358e9e03f0401bc
Sakshi Dipak Kachare <sakshikachare23@gmail.com>
2026-09-24 00:24:04 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program775.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class program775
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        HashMap <Character , Integer> hobj = new HashMap<Character , Integer>();
+
+        System.out.println("Enter String :");
+        String str = sobj.nextLine();
+
+        char Arr[] = str.toCharArray();
+
+        int iCount = 0;
+        
+        for(char ch : Arr)
+        {
+            if(hobj.containsKey(ch))
+            {
+                iCount = hobj.get(ch);
+                hobj.put(ch,iCount+1);
+                iCount++;    
+            }
+            else
+            {
+                hobj.put(ch,1 );
+            }
+        }
+        
+        int iMax = 0;
+        char temp = '\0';
+
+        for(char cValue : hobj.keySet())
+        {
+            if(hobj.get(cValue) > iMax)
+            {
+                iMax = hobj.get(cValue);
+                temp = cValue;
+            }
+           
+        }
+        System.out.println(temp+" Occurs maximum times ie:"+iMax);
+    }
+}
