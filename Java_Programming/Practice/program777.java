afa07f70489bf1b5a72f398b5ac43ebb4cc98b27
Sakshi Dipak Kachare <sakshikachare23@gmail.com>
2026-09-26 00:11:11 +0530

    Problems on Collection

---
+++ Java_Programming/Practice/program777.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class program777
+{
+    public static void main(String A[])
+    {
+        HashMap <String , Integer>hobj = new HashMap<String , Integer>();
+
+        hobj.put("PPA",1);
+        hobj.put("LB",1);
+        hobj.put("Python",1);
+        hobj.put("LSP",1);
+        hobj.put("LB",2);
+
+        System.out.println(hobj);
+    }
+}
